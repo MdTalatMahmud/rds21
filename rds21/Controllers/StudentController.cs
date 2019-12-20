f8aab4eaaa6af69fa31a0f8fe0d9528858ebaa7b
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2019-12-20 22:41:58 +0600

    First commit

---
+++ rds21/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using rds21.Models;
+
+namespace rds21.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        [HttpGet]
+        public ActionResult StudentRegistration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult StudentRegistration(Student aStudent)
+        {
+            using (RDSDatabaseEntities db=new RDSDatabaseEntities())
+            {
+                db.Students.Add(aStudent);
+                db.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ rds21/Controllers/TeacherController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using rds21.Models;
+
+namespace rds21.Controllers
+{
+    public class TeacherController : Controller
+    {
+        // GET: Teacher
+        [HttpGet]
+        public ActionResult TeacherRegistration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult TeacherRegistration(Teacher aTeacher)
+        {
+            using (RDSDatabaseEntities db = new RDSDatabaseEntities())
+            {
+                db.Teachers.Add(aTeacher);
+                db.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
